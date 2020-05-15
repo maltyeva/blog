6e7839c10e504929409543f419e9ba3e702e77f1
Maria <maria.altyeva@gmail.com>
2020-05-15 09:35:36 +0800

    added active storage

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.6.3'
 
 gem 'simple_form'
 gem 'dotenv-rails', groups: [:development, :test]
+gem 'cloudinary', '~> 1.12.0'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -62,13 +62,13 @@ class ArticlesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def article_params
-      params.require(:article).permit(:title, :body)
-    end
+  # Only allow a list of trusted parameters through.
+  def article_params
+    params.require(:article).permit(:title, :body, photos: [])
+  end
 end
