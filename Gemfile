14875132f9f0754a5422c9e818fd8747a5371622
Maria <maria.altyeva@gmail.com>
2020-05-29 10:16:15 +0800

    added images

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ ruby '2.6.3'
 
 gem 'simple_form'
 
+gem 'cloudinary', '~> 1.12.0'
+
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.1'
 # Use postgresql as the database for Active Record
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -46,13 +46,13 @@ class ArticlesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def article_params
-      params.require(:article).permit(:title, :body)
-    end
+  # Only allow a trusted parameter "white list" through.
+  def article_params
+    params.require(:article).permit(:title, :body, :photo)
+  end
 end
